fe9fc004a8044c3a430a02c98693d41cccecffc4
sow4063 <sow4063@gmail.com>
2019-02-11 15:24:24 +0900

    msg line to fs

--- hub/hub.js
+++ hub/hub.js
@@ -10,6 +10,10 @@ const
   request = require('request'),
   app = express().use(bodyParser.json()); // creates express http server
 
+// Imports dependencies and set up http server
+const PAGE_ACCESS_TOKEN = "EAAEW3BK980wBAJqt80yIzcGMrjynNynA4OkZAaaXPSfEfZCf7rNbWpQuDX4rMbGfOxzpLzfwvyLAYwZBvlxdM9L0jhssZCBPN3CwkWYYS4zBguXWUlNLeyDcTZCl2MhiVqZC6FiG6vr6BQ2eZCY0THggbbr9HzZC7bQ4PEXBPNfR3GcFfZAoE3ZCNu";
+
+
 // configuration ===========================================
 const sslPath = '/etc/letsencrypt/live/www.fordicpro.io/';
 
@@ -29,6 +33,29 @@ app.post('/linemsg', (req, res) => {
   let body = req.body;
 
   console.log('body from LINEBOT : ', body);
+
+  // send to the fsbot
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": '1172651839508093'
+    },
+    "message": body.message
+  }
+  
+  // Send the HTTP request to the Facebook Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
 
 });
 
