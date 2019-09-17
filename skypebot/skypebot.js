d89c06169e623f71eb191d990c9b7c6c7b20746c
sow4063 <sow4063@gmail.com>
2019-09-17 21:32:17 +0900

    modify https options

--- skypebot/skypebot.js
+++ skypebot/skypebot.js
@@ -12,7 +12,7 @@ const sslPath = '/etc/letsencrypt/live/www.fordicpro.io/';
 
 const options = {  
    key: fs.readFileSync(sslPath + 'privkey.pem'),
-   cert: fs.readFileSync(sslPath + 'fullchain.pem'),
+   certificate: fs.readFileSync(sslPath + 'fullchain.pem'),
    url: 'www.fordicpro.io',
    name: 'skypebot'
  };
